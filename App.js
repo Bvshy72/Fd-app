fc30326dce2aa01c82b8ef797ca1ac0782f89ad7
Bvshy72 <167409816+Bvshy72@users.noreply.github.com>
2024-10-15 20:03:40 +0530

    Update App.js

--- App.js
+++ App.js
@@ -2,8 +2,9 @@ import React from 'react';
 import {StyleSheet, Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import LoginScreen from './LoginScreen'; // Adjust the import path as needed
-import RestaurantScreen from './RestaurantScreen'; // Adjust the import path as needed
+import LoginScreen from './LoginScreen';
+import RestaurantScreen from './RestaurantScreen'; 
+import BillScreen from './BillScreen'; 
 
 const Stack = createNativeStackNavigator();
 
@@ -14,6 +15,7 @@ const App = () => {
        
         <Stack.Screen name="Login" component={LoginScreen} options={{ headerShown: false }}/>
         <Stack.Screen name="Restaurants" component={RestaurantScreen} options={{ title: 'Restaurants' }}/>
+        <Stack.Screen name="Bill" component={BillScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
