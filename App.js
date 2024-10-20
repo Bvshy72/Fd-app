3d3c68b8bdb497803d5f68906a2d8aff938ecea8
Bvshy72 <167409816+Bvshy72@users.noreply.github.com>
2024-10-20 21:58:46 +0530

    App.js (20/10)

--- App.js
+++ App.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import {StyleSheet, Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import LoginScreen from './LoginScreen';
-import RestaurantScreen from './RestaurantScreen'; 
-import BillScreen from './BillScreen'; 
+import RestaurantScreen from './RestaurantScreen';
+import LocationScreen from './LocationScreen'; 
+import OrderScreen from './OrderScreen';
 
 const Stack = createNativeStackNavigator();
 
@@ -12,10 +12,10 @@ const App = () => {
   return (
     <NavigationContainer>
       <Stack.Navigator initialRouteName="Login">
-       
-        <Stack.Screen name="Login" component={LoginScreen} options={{ headerShown: false }}/>
-        <Stack.Screen name="Restaurants" component={RestaurantScreen} options={{ title: 'Restaurants' }}/>
-        <Stack.Screen name="Bill" component={BillScreen} />
+        <Stack.Screen name="Login" component={LoginScreen} />
+        <Stack.Screen name="Restaurants" component={RestaurantScreen} />
+        <Stack.Screen name="Location" component={LocationScreen} />
+        <Stack.Screen name="Orders" component={OrderScreen} />    
       </Stack.Navigator>
     </NavigationContainer>
   );
